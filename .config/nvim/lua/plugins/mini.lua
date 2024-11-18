79fac2eaf9e4db03f1c62cb3a2e9453e033d012c
AirKoala <airkoala1@gmail.com>
2024-11-19 10:28:59 +1100

    Update plugins.

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,7 @@
+return {
+  'echasnovski/mini.nvim',
+  version = '*',
+  init = function()
+    require('mini.icons').setup()
+  end,
+}
--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -1,27 +1,41 @@
 return {
   "karb94/neoscroll.nvim",
   config = function()
-    require('neoscroll').setup({
+    local neoscroll = require('neoscroll')
+
+    neoscroll.setup({
       easing_function = "quadratic",
     })
 
     local duration = 50
 
-    local t = tostring(duration)
-    local two_t = tostring(duration * 2)
+    local t = 50
+    local two_t = duration * 2
+
+    local s_opt = {
+      move_cursor = true;
+      duration = t;
+      half_win_duration = t;
+      easing = 'quadratic';
+    }
+
+    local two_s_opt = s_opt
+    two_s_opt.duration = two_s_opt.duration * 2
 
     local map    = {}
     -- Syntax: t[keys] = {function, {function arguments}}
-    map['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', t } }
-    map['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', t } }
-    map['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', two_t } }
-    map['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', two_t } }
-    -- map['<C-y>'] = nil
-    -- map['<C-e>'] = nil
-    map['zt']    = { 'zt', { t } }
-    map['zz']    = { 'zz', { t } }
-    map['zb']    = { 'zb', { t } }
+    map['<C-u>'] = function() neoscroll.ctrl_u(s_opt) end
+    map['<C-d>'] = function() neoscroll.ctrl_d(s_opt) end
+    map['<C-b>'] = function() neoscroll.ctrl_b(two_s_opt) end
+    map['<C-f>'] = function() neoscroll.ctrl_f(two_s_opt) end
 
-    require('neoscroll.config').set_mappings(map)
+    map['zt']    = function() neoscroll.zt(s_opt) end
+    map['zz']    = function() neoscroll.zz(s_opt) end
+    map['zb']    = function() neoscroll.zb(s_opt) end
+
+    local modes  = { 'n', 'v', 'x' }
+    for key, func in pairs(map) do
+      vim.keymap.set(modes, key, func)
+    end
   end
 }

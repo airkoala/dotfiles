f3ed8dd77370df96ff601477b2e4621ddcf81f05
AirKoala <airkoala1@gmail.com>
2024-12-28 07:45:41 +1100

    Add comment bind descriptions.

--- .config/nvim/lua/plugins/comment-nvim.lua
+++ .config/nvim/lua/plugins/comment-nvim.lua
@@ -6,8 +6,13 @@ return {
     comment.setup({});
 
     -- Duplicate line and comment it
-    vim.keymap.set('n', 'gyc', 'yy<Plug>(comment_toggle_linewise_current)p', { noremap = true, silent = true });
+    vim.keymap.set('n', 'gyc', 'yy<Plug>(comment_toggle_linewise_current)p', {
+      noremap = true,
+      silent = true,
+      desc = "Duplicate line and comment it"
+    });
     -- Duplicate block and comment it
-    vim.keymap.set('v', 'gy', "Vygv<Plug>(comment_toggle_linewise_visual)'>p", { noremap = true, silent = true });
+    vim.keymap.set('v', 'gy', "Vygv<Plug>(comment_toggle_linewise_visual)'>p",
+      { noremap = true, silent = true, desc = "Duplicate block and comment it" });
   end,
 };

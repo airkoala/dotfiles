a50deb8e401ff9d155fb23dc6872ba0ae1a99807
AirKoala <airkoala1@gmail.com>
2025-01-11 22:25:42 +1100

    Add tiny-inline-diagnostics plugin.

---
+++ .config/nvim/lua/plugins/tiny-inline-diagnostics.lua
@@ -0,0 +1,12 @@
+return {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "LspAttach",
+  priority = 1000,
+  config = function()
+    require("tiny-inline-diagnostic").setup({
+      options = {
+        multilines = true }
+    })
+    vim.diagnostic.config({ virtual_text = false })
+  end,
+}

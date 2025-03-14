d8490aa651cd9b318a27e12ebf05bfc528420dde
AirKoala <airkoala1@gmail.com>
2025-03-14 19:10:54 +1100

    Improve brightness control args syntax

--- Scripts/monitor-controller/run.py
+++ Scripts/monitor-controller/run.py
@@ -1,15 +1,19 @@
 from monitorcontrol import get_monitors
+from sys import argv
+
+print(argv)
+
+if len(argv) <= 1:
+    for i, monitor in enumerate(get_monitors()):
+        with monitor:
+            print(f"Monitor {i+1}, {monitor.get_luminance()}%")
+    exit()
 
 for i, monitor in enumerate(get_monitors()):
     with monitor:
-        print(f"Monitor {i+1}, {monitor.get_luminance()}%")
-
-        new_brightness = input("New brightness: ")
-
-        if new_brightness != "":
-            try:
-                monitor.set_luminance(int(new_brightness))
-            except ValueError:
-                print("Invalid brightness value")
-
-        print("")
+        new_brightness = argv[i+1] if i+1 < len(argv) else argv[-1]
+        try:
+            monitor.set_luminance(int(new_brightness))
+            print(f"Monitor {i+1}, {monitor.get_luminance()}%")
+        except ValueError:
+            print("Invalid brightness value", argv[1:])

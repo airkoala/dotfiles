b84701a722ad15de93bfa04af8588f8df9143fc8
AirKoala <airkoala1@gmail.com>
2025-06-22 06:57:13 +1000

    Add gruvbox colours to oil-git-status

--- .config/nvim/lua/plugins/oil-git-status.lua
+++ .config/nvim/lua/plugins/oil-git-status.lua
@@ -4,5 +4,23 @@ return {
   dependencies = {
     "stevearc/oil.nvim",
   },
-  config = true,
+  config = function()
+    local ogs = require("oil-git-status")
+    local palette = require("gruvbox").palette
+
+    ogs.setup({})
+
+    for _, hl_group in pairs(ogs.highlight_groups) do
+      -- gruvbox dark hard background
+      vim.api.nvim_set_hl(0, hl_group.hl_group, { bg = palette.dark0_hard })
+
+      if hl_group.index then
+        -- index changes
+        vim.api.nvim_set_hl(0, hl_group.hl_group, { fg = palette.bright_blue })
+      else
+        -- working tree changes
+        vim.api.nvim_set_hl(0, hl_group.hl_group, { fg = palette.bright_yellow })
+      end
+    end
+  end,
 }
